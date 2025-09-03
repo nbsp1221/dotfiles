a7ade7a58cc893f484b5e406773bd7d3229ec80f
Naki <nbsp1221@gmail.com>
2025-09-03 17:14:01 +0900

    ✨ [Ubuntu] Add GitHub CLI installation to setup script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -93,6 +93,19 @@ success "Programming languages and runtime installation completed."
 
 log "Installing useful utilities..."
 
+# Install GitHub CLI
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian
+command -v gh > /dev/null 2>&1 || {
+  sudo mkdir -p -m 755 /etc/apt/keyrings
+  out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+  cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+  sudo mkdir -p -m 755 /etc/apt/sources.list.d
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  sudo apt update
+  sudo apt install -y gh
+}
+
 # Install Docker
 command -v docker > /dev/null 2>&1 || {
   sudo install -m 0755 -d /etc/apt/keyrings
